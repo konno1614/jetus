8cfe1aef90b085f4af6afb0716c47d0caba1bb9a
konno1614 <konno1614@gmail.com>
2024-07-26 21:25:53 +0900

    エフェクト付与

--- src/js/lib/effect.js
+++ src/js/lib/effect.js
@@ -17,4 +17,18 @@ $(function() {
         }
     });
 
+    // fadein
+    $(window).scroll(function() {
+        const wHeight = $(window).height();
+        const wScroll = $(window).scrollTop();
+            $(".js-fadein").each(function () {
+                const bPosition = $(this).offset().top;
+            if (wScroll > bPosition - wHeight + 200) {
+                $(this).addClass("is-active");
+            }
+        });
+    });
+
+    // fadeup
+    $('.js-fadeup span').delay(500).addClass('is-active');
 });
